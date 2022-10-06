d9ed0114ea14671cfd0c8908fe210d59eabad220
Kledson Basso <kledsonhugo@gmail.com>
2022-10-06 15:43:22 -0300

    Organizing Repo Strucure

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "tf-state-nagioscore-bucket"
+    bucket         = "tf-state-nagioscore-multiaz-bucket"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "tf-state-nagioscore-table"
