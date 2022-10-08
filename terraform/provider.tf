f1729d1ef8ca905e90b5dd6053cb76bda9d079dd
Kledson Basso <kledsonhugo@gmail.com>
2022-10-07 21:56:14 -0300

    Update Required Version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,12 @@
 # PROVIDER
 terraform {
 
+  required_version = "~> 1.3.1"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.34"
     }
   }
 
@@ -12,7 +14,7 @@ terraform {
     bucket         = "tf-state-nagioscore-multiaz-bucket"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "tf-state-nagioscore-table"
+    dynamodb_table = "tf-state-nagioscore-multiaz-table"
   }
 
 }
