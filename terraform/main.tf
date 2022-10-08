b9cc66a3549e25be6966923a783160656c15d76b
Kledson Basso <kledsonhugo@gmail.com>
2022-10-07 22:36:03 -0300

    Added Plan File into Pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,26 +8,26 @@ resource "aws_vpc" "vpc1" {
     }
 }
 
-resource "aws_vpc" "vpc2" {
-    cidr_block           = "40.0.0.0/16"
-    enable_dns_hostnames = "true"
+# resource "aws_vpc" "vpc2" {
+#     cidr_block           = "40.0.0.0/16"
+#     enable_dns_hostnames = "true"
 
-    tags = {
-        Name = "vpc2"
-    }
-}
+#     tags = {
+#         Name = "vpc2"
+#     }
+# }
 
-# VPC Peering
-resource "aws_vpc_peering_connection" "vpc_peering" {
-    peer_vpc_id   = aws_vpc.vpc2.id
-    vpc_id        = aws_vpc.vpc1.id
-    auto_accept   = true
+# # VPC Peering
+# resource "aws_vpc_peering_connection" "vpc_peering" {
+#     peer_vpc_id   = aws_vpc.vpc2.id
+#     vpc_id        = aws_vpc.vpc1.id
+#     auto_accept   = true
     
-    tags = {
-        Name = "vpc_peering"
-    }
+#     tags = {
+#         Name = "vpc_peering"
+#     }
 
-}
+# }
 
 # INTERNET GATEWAY
 resource "aws_internet_gateway" "igw_vpc1" {
@@ -38,13 +38,13 @@ resource "aws_internet_gateway" "igw_vpc1" {
     }
 }
 
-resource "aws_internet_gateway" "igw_vpc2" {
-    vpc_id = aws_vpc.vpc2.id
+# resource "aws_internet_gateway" "igw_vpc2" {
+#     vpc_id = aws_vpc.vpc2.id
 
-    tags = {
-        Name = "igw_vpc2"
-    }
-}
+#     tags = {
+#         Name = "igw_vpc2"
+#     }
+# }
 
 # SUBNET
 resource "aws_subnet" "sn_vpc1_public" {
@@ -68,67 +68,67 @@ resource "aws_subnet" "sn_vpc1_private" {
     }
 }
 
-resource "aws_subnet" "sn_vpc2_public" {
-    vpc_id                  = aws_vpc.vpc2.id
-    cidr_block              = "40.0.1.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone       = "us-east-1a"
+# resource "aws_subnet" "sn_vpc2_public" {
+#     vpc_id                  = aws_vpc.vpc2.id
+#     cidr_block              = "40.0.1.0/24"
+#     map_public_ip_on_launch = "true"
+#     availability_zone       = "us-east-1a"
 
-    tags = {
-        Name = "sn_vpc2_public"
-    }
-}
+#     tags = {
+#         Name = "sn_vpc2_public"
+#     }
+# }
 
-resource "aws_subnet" "sn_vpc2_private" {
-    vpc_id            = aws_vpc.vpc2.id
-    cidr_block        = "40.0.2.0/24"
-    availability_zone = "us-east-1c"
+# resource "aws_subnet" "sn_vpc2_private" {
+#     vpc_id            = aws_vpc.vpc2.id
+#     cidr_block        = "40.0.2.0/24"
+#     availability_zone = "us-east-1c"
 
-    tags = {
-        Name = "sn_vpc2_private"
-    }
-}
+#     tags = {
+#         Name = "sn_vpc2_private"
+#     }
+# }
 
-# Elastic IP
-resource "aws_eip" "eip_ngw_vpc1" {
-    depends_on = [aws_internet_gateway.igw_vpc1]
-}
+# # Elastic IP
+# resource "aws_eip" "eip_ngw_vpc1" {
+#     depends_on = [aws_internet_gateway.igw_vpc1]
+# }
 
-resource "aws_eip" "eip_ngw_vpc2" {
-    depends_on = [aws_internet_gateway.igw_vpc2]
-}
+# resource "aws_eip" "eip_ngw_vpc2" {
+#     depends_on = [aws_internet_gateway.igw_vpc2]
+# }
 
-# NAT Gateway
-resource "aws_nat_gateway" "ngw_vpc1" {
-    allocation_id = aws_eip.eip_ngw_vpc1.id
-    subnet_id     = aws_subnet.sn_vpc1_public.id
+# # NAT Gateway
+# resource "aws_nat_gateway" "ngw_vpc1" {
+#     allocation_id = aws_eip.eip_ngw_vpc1.id
+#     subnet_id     = aws_subnet.sn_vpc1_public.id
 
-    tags = {
-        Name = "ngw_vpc1"
-    }
+#     tags = {
+#         Name = "ngw_vpc1"
+#     }
 
-    depends_on = [aws_internet_gateway.igw_vpc1]
-}
+#     depends_on = [aws_internet_gateway.igw_vpc1]
+# }
 
-resource "aws_nat_gateway" "ngw_vpc2" {
-    allocation_id = aws_eip.eip_ngw_vpc2.id
-    subnet_id     = aws_subnet.sn_vpc2_public.id
+# resource "aws_nat_gateway" "ngw_vpc2" {
+#     allocation_id = aws_eip.eip_ngw_vpc2.id
+#     subnet_id     = aws_subnet.sn_vpc2_public.id
 
-    tags = {
-        Name = "ngw_vpc2"
-    }
+#     tags = {
+#         Name = "ngw_vpc2"
+#     }
 
-    depends_on = [aws_internet_gateway.igw_vpc2]
-}
+#     depends_on = [aws_internet_gateway.igw_vpc2]
+# }
 
 # ROUTE TABLE
 resource "aws_route_table" "rt_vpc1_public" {
     vpc_id = aws_vpc.vpc1.id
 
-    route {
-        cidr_block = "40.0.0.0/16"
-        gateway_id = aws_vpc_peering_connection.vpc_peering.id
-    }
+    # route {
+    #     cidr_block = "40.0.0.0/16"
+    #     gateway_id = aws_vpc_peering_connection.vpc_peering.id
+    # }
 
     route {
         cidr_block = "0.0.0.0/0"
@@ -143,56 +143,56 @@ resource "aws_route_table" "rt_vpc1_public" {
 resource "aws_route_table" "rt_vpc1_private" {
     vpc_id = aws_vpc.vpc1.id
 
-    route {
-        cidr_block = "40.0.0.0/16"
-        gateway_id = aws_vpc_peering_connection.vpc_peering.id
-    }
+    # route {
+    #     cidr_block = "40.0.0.0/16"
+    #     gateway_id = aws_vpc_peering_connection.vpc_peering.id
+    # }
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.ngw_vpc1.id
-    }
+    # route {
+    #     cidr_block = "0.0.0.0/0"
+    #     gateway_id = aws_nat_gateway.ngw_vpc1.id
+    # }
 
     tags = {
         Name = "rt_vpc1_private"
     }
 }
 
-resource "aws_route_table" "rt_vpc2_public" {
-    vpc_id = aws_vpc.vpc2.id
+# resource "aws_route_table" "rt_vpc2_public" {
+#     vpc_id = aws_vpc.vpc2.id
 
-    route {
-        cidr_block = "30.0.0.0/16"
-        gateway_id = aws_vpc_peering_connection.vpc_peering.id
-    }
+#     route {
+#         cidr_block = "30.0.0.0/16"
+#         gateway_id = aws_vpc_peering_connection.vpc_peering.id
+#     }
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw_vpc2.id
-    }
+#     route {
+#         cidr_block = "0.0.0.0/0"
+#         gateway_id = aws_internet_gateway.igw_vpc2.id
+#     }
 
-    tags = {
-        Name = "rt_vpc2_public"
-    }
-}
+#     tags = {
+#         Name = "rt_vpc2_public"
+#     }
+# }
 
-resource "aws_route_table" "rt_vpc2_private" {
-    vpc_id = aws_vpc.vpc2.id
+# resource "aws_route_table" "rt_vpc2_private" {
+#     vpc_id = aws_vpc.vpc2.id
 
-    route {
-        cidr_block = "30.0.0.0/16"
-        gateway_id = aws_vpc_peering_connection.vpc_peering.id
-    }
+#     route {
+#         cidr_block = "30.0.0.0/16"
+#         gateway_id = aws_vpc_peering_connection.vpc_peering.id
+#     }
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.ngw_vpc2.id
-    }
+#     route {
+#         cidr_block = "0.0.0.0/0"
+#         gateway_id = aws_nat_gateway.ngw_vpc2.id
+#     }
 
-    tags = {
-        Name = "rt_vpc2_private"
-    }
-}
+#     tags = {
+#         Name = "rt_vpc2_private"
+#     }
+# }
 
 # SUBNET ASSOCIATION
 resource "aws_route_table_association" "rt_vpc1_public_To_sn_vpc1_public" {
@@ -205,15 +205,15 @@ resource "aws_route_table_association" "rt_vpc1_private_To_sn_vpc1_private" {
   route_table_id = aws_route_table.rt_vpc1_private.id
 }
 
-resource "aws_route_table_association" "rt_vpc2_public_To_sn_vpc2_public" {
-  subnet_id      = aws_subnet.sn_vpc2_public.id
-  route_table_id = aws_route_table.rt_vpc2_public.id
-}
+# resource "aws_route_table_association" "rt_vpc2_public_To_sn_vpc2_public" {
+#   subnet_id      = aws_subnet.sn_vpc2_public.id
+#   route_table_id = aws_route_table.rt_vpc2_public.id
+# }
 
-resource "aws_route_table_association" "rt_vpc2_private_To_sn_vpc2_private" {
-  subnet_id      = aws_subnet.sn_vpc2_private.id
-  route_table_id = aws_route_table.rt_vpc2_private.id
-}
+# resource "aws_route_table_association" "rt_vpc2_private_To_sn_vpc2_private" {
+#   subnet_id      = aws_subnet.sn_vpc2_private.id
+#   route_table_id = aws_route_table.rt_vpc2_private.id
+# }
 
 # SECURITY GROUP
 resource "aws_security_group" "sg_vpc1_public" {
@@ -308,97 +308,97 @@ resource "aws_security_group" "sg_vpc1_private" {
     }
 }
 
-resource "aws_security_group" "sg_vpc2_public" {
-    name        = "sg_vpc2_public"
-    description = "sg_vpc2_public"
-    vpc_id      = aws_vpc.vpc2.id
+# resource "aws_security_group" "sg_vpc2_public" {
+#     name        = "sg_vpc2_public"
+#     description = "sg_vpc2_public"
+#     vpc_id      = aws_vpc.vpc2.id
     
-    egress {
-        description = "All to All"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+#     egress {
+#         description = "All to All"
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
 
-    ingress {
-        description = "All from 40.0.0.0/16"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["40.0.0.0/16"]
-    }
+#     ingress {
+#         description = "All from 40.0.0.0/16"
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = ["40.0.0.0/16"]
+#     }
 
-    ingress {
-        description = "All from 30.0.0.0/16"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["30.0.0.0/16"]
-    }
+#     ingress {
+#         description = "All from 30.0.0.0/16"
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = ["30.0.0.0/16"]
+#     }
 
-    ingress {
-        description = "TCP/22 from All"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+#     ingress {
+#         description = "TCP/22 from All"
+#         from_port   = 22
+#         to_port     = 22
+#         protocol    = "tcp"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
     
-    ingress {
-        description = "TCP/80 from All"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+#     ingress {
+#         description = "TCP/80 from All"
+#         from_port   = 80
+#         to_port     = 80
+#         protocol    = "tcp"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
 
-    ingress {
-        description = "TCP/3389 from All"
-        from_port   = 3389
-        to_port     = 3389
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+#     ingress {
+#         description = "TCP/3389 from All"
+#         from_port   = 3389
+#         to_port     = 3389
+#         protocol    = "tcp"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
 
-    tags = {
-        Name = "sg_vpc2_public"
-    }
-}
+#     tags = {
+#         Name = "sg_vpc2_public"
+#     }
+# }
 
-resource "aws_security_group" "sg_vpc2_private" {
-    name        = "sg_vpc2_private"
-    description = "sg_vpc2_private"
-    vpc_id      = aws_vpc.vpc2.id
+# resource "aws_security_group" "sg_vpc2_private" {
+#     name        = "sg_vpc2_private"
+#     description = "sg_vpc2_private"
+#     vpc_id      = aws_vpc.vpc2.id
     
-    egress {
-        description = "All to All"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+#     egress {
+#         description = "All to All"
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
 
-    ingress {
-        description = "All from 40.0.0.0/16"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["40.0.0.0/16"]
-    }
+#     ingress {
+#         description = "All from 40.0.0.0/16"
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = ["40.0.0.0/16"]
+#     }
 
-    ingress {
-        description = "All from 30.0.0.0/16"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["30.0.0.0/16"]
-    }
+#     ingress {
+#         description = "All from 30.0.0.0/16"
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = ["30.0.0.0/16"]
+#     }
 
-    tags = {
-        Name = "sg_vpc2_private"
-    }
-}
+#     tags = {
+#         Name = "sg_vpc2_private"
+#     }
+# }
 
 # EC2 INSTANCE
 resource "aws_instance" "nagios" {
@@ -482,34 +482,34 @@ resource "aws_instance" "node_a" {
     }
 }
 
-resource "aws_instance" "node_b" {
-    ami                    = "ami-02e136e904f3da870"
-    instance_type          = "t2.micro"
-    subnet_id              = aws_subnet.sn_vpc2_public.id
-    vpc_security_group_ids = [aws_security_group.sg_vpc2_public.id]
-	user_data = <<-EOF
-        #!/bin/bash
+# resource "aws_instance" "node_b" {
+#     ami                    = "ami-02e136e904f3da870"
+#     instance_type          = "t2.micro"
+#     subnet_id              = aws_subnet.sn_vpc2_public.id
+#     vpc_security_group_ids = [aws_security_group.sg_vpc2_public.id]
+# 	user_data = <<-EOF
+#         #!/bin/bash
 
-        # NCPA Agent Install instructions
-        # https://assets.nagios.com/downloads/ncpa/docs/Installing-NCPA.pdf
-        yum update -y
-        rpm -Uvh https://assets.nagios.com/downloads/ncpa/ncpa-latest.el7.x86_64.rpm
-        systemctl restart ncpa_listener.service
-        echo done > /tmp/ncpa-agent.done
+#         # NCPA Agent Install instructions
+#         # https://assets.nagios.com/downloads/ncpa/docs/Installing-NCPA.pdf
+#         yum update -y
+#         rpm -Uvh https://assets.nagios.com/downloads/ncpa/ncpa-latest.el7.x86_64.rpm
+#         systemctl restart ncpa_listener.service
+#         echo done > /tmp/ncpa-agent.done
 
-        # SNMP Agent install instructions
-        # https://www.site24x7.com/help/admin/adding-a-monitor/configuring-snmp-linux.html
-        yum update -y
-        yum install net-snmp -y
-        echo "rocommunity public" >> /etc/snmp/snmpd.conf
-        service snmpd restart
-        echo done > /tmp/snmp-agent.done
-	EOF
+#         # SNMP Agent install instructions
+#         # https://www.site24x7.com/help/admin/adding-a-monitor/configuring-snmp-linux.html
+#         yum update -y
+#         yum install net-snmp -y
+#         echo "rocommunity public" >> /etc/snmp/snmpd.conf
+#         service snmpd restart
+#         echo done > /tmp/snmp-agent.done
+# 	EOF
 
-    tags = {
-        Name = "node_b"
-    }
-}
+#     tags = {
+#         Name = "node_b"
+#     }
+# }
 
 resource "aws_instance" "node_c" {
     ami                    = "ami-02e136e904f3da870"
@@ -540,32 +540,32 @@ resource "aws_instance" "node_c" {
     }
 }
 
-resource "aws_instance" "node_d" {
-    ami                    = "ami-02e136e904f3da870"
-    instance_type          = "t2.micro"
-    subnet_id              = aws_subnet.sn_vpc2_private.id
-    vpc_security_group_ids = [aws_security_group.sg_vpc2_private.id]
-	user_data = <<-EOF
-        #!/bin/bash
+# resource "aws_instance" "node_d" {
+#     ami                    = "ami-02e136e904f3da870"
+#     instance_type          = "t2.micro"
+#     subnet_id              = aws_subnet.sn_vpc2_private.id
+#     vpc_security_group_ids = [aws_security_group.sg_vpc2_private.id]
+# 	user_data = <<-EOF
+#         #!/bin/bash
 
-        # NCPA Agent Install instructions
-        # https://assets.nagios.com/downloads/ncpa/docs/Installing-NCPA.pdf
-        yum update -y
-        rpm -Uvh https://assets.nagios.com/downloads/ncpa/ncpa-latest.el7.x86_64.rpm
-        systemctl restart ncpa_listener.service
-        echo done > /tmp/ncpa-agent.done
+#         # NCPA Agent Install instructions
+#         # https://assets.nagios.com/downloads/ncpa/docs/Installing-NCPA.pdf
+#         yum update -y
+#         rpm -Uvh https://assets.nagios.com/downloads/ncpa/ncpa-latest.el7.x86_64.rpm
+#         systemctl restart ncpa_listener.service
+#         echo done > /tmp/ncpa-agent.done
 
-        # SNMP Agent install instructions
-        # https://www.site24x7.com/help/admin/adding-a-monitor/configuring-snmp-linux.html
-        yum update -y
-        yum install net-snmp -y
-        echo "rocommunity public" >> /etc/snmp/snmpd.conf
-        service snmpd restart
-        echo done > /tmp/snmp-agent.done
-        key_name = "nagios"
-	EOF
+#         # SNMP Agent install instructions
+#         # https://www.site24x7.com/help/admin/adding-a-monitor/configuring-snmp-linux.html
+#         yum update -y
+#         yum install net-snmp -y
+#         echo "rocommunity public" >> /etc/snmp/snmpd.conf
+#         service snmpd restart
+#         echo done > /tmp/snmp-agent.done
+#         key_name = "nagios"
+# 	EOF
  
-    tags = {
-        Name = "node_d"
-    }
-}
+#     tags = {
+#         Name = "node_d"
+#     }
+# }
