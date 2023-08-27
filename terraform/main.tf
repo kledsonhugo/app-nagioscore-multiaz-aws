61ff9f52bbc1c22530ee13fdc5634f9eff04b294
Kledson Basso <kledsonhugo@gmail.com>
2023-08-27 19:52:24 -0300

    GitHub Actions pipeline setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,7 +60,7 @@ resource "aws_subnet" "sn_vpc10_public" {
 
 resource "aws_subnet" "sn_vpc10_private" {
     vpc_id            = aws_vpc.vpc10.id
-    cidr_block        = "20.0.2.0/24"
+    cidr_block        = "10.0.2.0/24"
     availability_zone = "us-east-1c"
 
     tags = {
