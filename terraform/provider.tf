9e1cfa09de51b80320747be32404ede1844ce677
Kledson Basso <kledsonhugo@gmail.com>
2023-08-27 23:18:27 -0300

    Terraform apply

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,16 +10,10 @@ terraform {
     }
   }
 
-  variable "region" {
-    type    = string
-    default = "us-east-1"
-  }
-
   backend "s3" {
     bucket         = "app-nagios-core-multiaz-v1"
     key            = "terraform.tfstate"
     dynamodb_table = "app-nagios-core-multiaz-v1"
-    region         = "${var.region}"
   }
 
 }
